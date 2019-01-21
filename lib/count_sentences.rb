d65567baed428fba02a0bbb0e5dc7056b9a08a5a
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-21 02:54:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,6 +28,6 @@ class String
 #returns the number of sentences in a complex string
 
   def count_sentences
-    self.split(/\w+[\.|\!\?]/).count
+    self.split(/\w+[\.|\!\?]/).delete(1)
   end
 end
