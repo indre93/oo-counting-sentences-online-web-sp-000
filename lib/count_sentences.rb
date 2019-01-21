219d62cdd2162e4e0760c1161526f4b904e0311e
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-21 02:07:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,7 +6,7 @@ class String
 #returns false if the string that you are calling this method on does NOT end in a period.
 
   def sentence?
-    " ".end_with?(".")
+    self.end_with?(".")
   end
 
 #returns true if the string that you are calling this method on ends in a question mark.
