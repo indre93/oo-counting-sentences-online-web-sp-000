539f5c917f638d316e93485149659bd394f9b2da
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-21 00:45:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
+  
 #returns true if the string that you are calling this method on ends in a period.
-#returns true if the string that you are calling this method on ends in a period.
+#returns false if the string that you are calling this method on does NOT end in a period.
 
   def sentence?
 
   end
+  
 #returns true if the string that you are calling this method on ends in a question mark.
-#
+#returns false if the string that you are calling this method on does NOT end in question mark.
 
   def question?
 
   end
-#
-#
+  
+#returns true if the string that you are calling this method on ends in an exclamation mark
+#returns false if the string that you are calling this method on does NOT end in a exclamation mark.
 
   def exclamation?
 
